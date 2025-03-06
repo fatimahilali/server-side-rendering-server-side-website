a2222a0e5bd62b11186bf03528b2df8f2b334fd7
Fatima El Hlali <elhilali-1991@hotmail.com>
2025-03-06 21:53:06 +0100

    #1 CSS aangepast en oneindig scrollen toegevoegd

---
+++ public/scripts/script.js
@@ -0,0 +1,18 @@
+/**
+ * @author Fatima
+ * @description Oneindig scrollen in een galerij door afbeeldingen te dupliceren.
+ * @file script.js
+ */
+
+// Functie om afbeeldingen te dupliceren en toe te voegen aan de galerij
+function voegAfbeeldingenToe() {
+  document.querySelector(".gallery").innerHTML += document.querySelector(".gallery").innerHTML;
+}
+
+// Luister naar het scrollen van de gebruiker
+window.addEventListener("scroll", function() {
+  // Controleer of de gebruiker bijna onderaan de pagina is
+  if (window.scrollY + window.innerHeight >= document.body.scrollHeight - 100) {
+      voegAfbeeldingenToe(); // Voeg nieuwe afbeeldingen toe
+  }
+});
